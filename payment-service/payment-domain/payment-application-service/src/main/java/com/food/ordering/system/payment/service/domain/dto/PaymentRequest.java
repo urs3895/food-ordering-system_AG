668e1f764b413069b5b11f569c2033bfe533b30d
urs3895 <53833643+urs3895@users.noreply.github.com>
2025-05-18 21:21:21 +0530

    Add files via upload

---
+++ payment-service/payment-domain/payment-application-service/src/main/java/com/food/ordering/system/payment/service/domain/dto/PaymentRequest.java
@@ -0,0 +1,26 @@
+package com.food.ordering.system.payment.service.domain.dto;
+
+
+import com.food.ordering.system.domain.valueobject.PaymentOrderStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class PaymentRequest {
+    private String id;
+    private String sagaId;
+    private String orderId;
+    private String customerId;
+    private BigDecimal price;
+    private Instant createdAt;
+    @Setter
+    private PaymentOrderStatus paymentOrderStatus;
+
+}
