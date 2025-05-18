a58d6c1c36f31bbf367db7a9aa7b83f0fa7a3f10
urs3895 <53833643+urs3895@users.noreply.github.com>
2025-05-18 22:05:41 +0530

    Add files via upload

---
+++ order-services/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/outbox/model/payment/OrderPaymentOutboxMessage.java
@@ -0,0 +1,33 @@
+package com.food.ordering.system.order.service.domain.outbox.model.payment;
+
+
+import com.food.ordering.system.outbox.OutboxStatus;
+import com.food.ordering.system.saga.SagaStatus;
+import com.food.ordering.system.domain.valueobject.OrderStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class OrderPaymentOutboxMessage {
+    private UUID id;
+    private UUID sagaId;
+    private ZonedDateTime createdAt;
+    @Setter
+    private ZonedDateTime processedAt;
+    private String type;
+    private String payload;
+    @Setter
+    private SagaStatus sagaStatus;
+    @Setter
+    private OrderStatus orderStatus;
+    @Setter
+    private OutboxStatus outboxStatus;
+    private int version;
+}
